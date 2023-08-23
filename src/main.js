beb33c5b2751d6457693d16b5dd016d99666203f
fnascimentosilva <fabricionsilva26@gmail.com>
2023-08-23 10:37:55 +0100

    instalação e configuração vuetify

--- src/main.js
+++ src/main.js
@@ -3,4 +3,24 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//rotas
+import {routes} from '@/router/routes.js'
+
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify ({
+    components,
+    directives,
+    icons: {
+        defaulSet: 'mdi'
+    }
+})
+
+createApp(App)
+.use(routes)
+.use(vuetify)
+.mount('#app')
